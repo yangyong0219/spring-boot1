429dcff14d613df19efccba4b5695f04144e43ec
yangyong <yangyong19@sinotrans.com>
2021-11-29 10:36:45 +0800

    ...

---
+++ demo/leetcode-server/src/main/java/com/example/leetcodeserver/LeetcodeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeServerApplication.class, args);
+    }
+
+}
---
+++ demo/leetcode-server/src/test/java/com/example/leetcodeserver/LeetcodeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
